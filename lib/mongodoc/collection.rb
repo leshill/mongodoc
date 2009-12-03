601a7605f44be189923c35ec1a2cf7901d73566a
Les Hill <leshill@gmail.com>
2009-12-02 23:08:11 -0500

    Use wrapped cursor from wrapped collection
    * Refactor collection spec

--- lib/mongodoc/collection.rb
+++ lib/mongodoc/collection.rb
@@ -3,14 +3,20 @@ require 'mongodoc/cursor'
 module MongoDoc
   class Collection
     attr_accessor :_collection
-    delegate :[], :clear, :count, :create_index, :db, :drop, :drop_index, :drop_indexes, :group, :hint, :index_information, :name, :options, :remove, :to => :_collection
+    delegate :[], :clear, :count, :create_index, :db, :drop, :drop_index, :drop_indexes, :group, :hint, :index_information, :name, :options, :remove, :rename, :size, :to => :_collection
     
     def initialize(name)
       self._collection = self.class.mongo_collection(name)
     end
     
-    def find(query = {}, options = {}, &block)
-      MongoDoc::BSON.decode(_collection.find(query, options, &block).to_a)
+    def find(query = {}, options = {})
+      cursor = MongoDoc::Cursor.new(_collection.find(query, options))
+      if block_given?
+        yield cursor
+        cursor.close
+      else
+        cursor
+      end
     end
     
     def find_one(spec_or_object_id = nil, options = {})
--- spec/collection_spec.rb
+++ spec/collection_spec.rb
@@ -25,27 +25,10 @@ describe "MongoDoc::Collection" do
       MongoDoc::Collection.new('collection_name')._collection.should == @mongo_collection
     end
   
-    %w([] clear count create_index db drop drop_index drop_indexes group hint index_information name options remove).each do |delegated_method|
+    %w([] clear count create_index db drop drop_index drop_indexes group hint index_information name options remove rename size).each do |delegated_method|
       it "delegates #{delegated_method} to the Mongo::Collection" do
         @mongo_collection.should_receive(delegated_method)
         MongoDoc::Collection.new('collection_name').send(delegated_method)
-      end
-    end
-    
-    context "#insert" do
-      it "delegates to the Mongo::Collection" do
-        doc = { 'sample' => 'data' }
-        options = {:safe => false}
-        @mongo_collection.should_receive(:insert).with(doc, options)
-        MongoDoc::Collection.new('collection_name').insert(doc, options)
-      end
-      
-      it "converts the doc_or_docs to bson" do
-        doc = { :sample => 'data' }
-        doc.should_receive(:to_bson)
-        options = {:safe => false}
-        @mongo_collection.stub(:insert)
-        MongoDoc::Collection.new('collection_name').insert(doc, options)
       end
     end
     
@@ -54,61 +37,104 @@ describe "MongoDoc::Collection" do
         @query = { 'sample' => 'data' }
         @options = {:limit => 1}
         @block = lambda {}
+        @cursor = stub('cursor', :close => nil)
+        @collection = MongoDoc::Collection.new('collection_name')
+        @mongo_collection.stub(:find).and_return(@cursor)
+        MongoDoc::Cursor.stub(:new).and_return(@cursor)
       end
       
       it "delegates to the Mongo::Collection" do
-        @mongo_collection.should_receive(:find).with(@query, @options, &@block)
-        MongoDoc::Collection.new('collection_name').find(@query, @options, &@block)
+        @mongo_collection.should_receive(:find).with(@query, @options, &@block).and_return(@cursor)
+        @collection.find(@query, @options, &@block)
       end
 
-      it "turns the cursor into an array" do
-        cursor = stub('cursor')
-        cursor.should_receive(:to_a)
-        @mongo_collection.stub(:find).and_return(cursor)
-        MongoDoc::Collection.new('collection_name').find(@query, @options, &@block)
+      it "wraps the cursor" do
+        MongoDoc::Cursor.should_receive(:new).with(@cursor).and_return(@cursor)
+        @collection.find(@query, @options, &@block)
       end
       
-      it "converts the array back from bson" do
-        results = [{'some' => 'results'}]
-        cursor = stub('cursor', :to_a => results)
-        @mongo_collection.stub(:find).and_return(cursor)
-        MongoDoc::BSON.should_receive(:decode).with(results)
-        MongoDoc::Collection.new('collection_name').find(@query, @options)
+      it "calls the block with a wrapped cursor" do
+        @collection.find(@query, @options) {|cursor| @result = cursor}
+        @result.should == @cursor
       end
     end
     
     context "#find_one" do
+      before do
+        @spec_or_object_id = { 'sample' => 'data' }
+        @options = {:limit => 1}
+        @collection = MongoDoc::Collection.new('collection_name')
+        @bson = stub('bson')
+        @mongo_collection.stub(:find_one).and_return(@bson)
+      end
+      
       it "delegates to the Mongo::Collection" do
-        spec_or_object_id = { 'sample' => 'data' }
-        options = {:limit => 1}
-        @mongo_collection.should_receive(:find_one).with(spec_or_object_id, options)
-        MongoDoc::Collection.new('collection_name').find_one(spec_or_object_id, options)
+        @mongo_collection.should_receive(:find_one).with(@spec_or_object_id, @options)
+        @collection.find_one(@spec_or_object_id, @options)
       end
 
       it "converts the result back from bson" do
-        spec_or_object_id = { 'sample' => 'data' }
-        options = {:limit => 1}
-        results = {'some' => 'result'}
-        @mongo_collection.stub(:find_one).and_return(results)
-        MongoDoc::BSON.should_receive(:decode).with(results)
-        MongoDoc::Collection.new('collection_name').find_one(spec_or_object_id, options)
+        MongoDoc::BSON.should_receive(:decode).with(@bson)
+        @collection.find_one(@spec_or_object_id, @options)
+      end
+
+      it "returns the converted result" do
+        obj = stub('obj')
+        MongoDoc::BSON.stub(:decode).and_return(obj)
+        @collection.find_one(@spec_or_object_id, @options).should == obj
+      end
+      
+      it "returns nil if the delegate returns nil" do
+        @mongo_collection.stub(:find_one)
+        @collection.find_one(@spec_or_object_id, @options).should be_nil
+      end
+    end
+
+    context "#insert" do
+      before do
+        @doc = { 'sample' => 'data' }
+        @options = {:safe => false}
+      end
+      
+      it "delegates to the Mongo::Collection" do
+        @mongo_collection.should_receive(:insert).with(@doc, @options)
+        MongoDoc::Collection.new('collection_name').insert(@doc, @options)
+      end
+      
+      it "converts the doc_or_docs to bson" do
+        @doc.should_receive(:to_bson)
+        @mongo_collection.stub(:insert)
+        MongoDoc::Collection.new('collection_name').insert(@doc, @options)
+      end
+
+      it "returns the delegates result" do
+        result = 'result'
+        @mongo_collection.stub(:insert).and_return(result)
+        MongoDoc::Collection.new('collection_name').insert(@doc, @options).should == result
       end
     end
 
     context "#save" do
+      before do
+        @doc = { 'sample' => 'data' }
+        @options = {:safe => false}
+      end
+      
       it "delegates to the Mongo::Collection" do
-        doc = { 'sample' => 'data' }
-        options = {:safe => false}
-        @mongo_collection.should_receive(:save).with(doc, options)
-        MongoDoc::Collection.new('collection_name').save(doc, options)
+        @mongo_collection.should_receive(:save).with(@doc, @options)
+        MongoDoc::Collection.new('collection_name').save(@doc, @options)
       end
 
       it "converts the doc to bson" do
-        doc = { :sample => 'data' }
-        options = {:safe => false}
-        doc.should_receive(:to_bson)
+        @doc.should_receive(:to_bson)
         @mongo_collection.stub(:save)
-        MongoDoc::Collection.new('collection_name').save(doc, options)
+        MongoDoc::Collection.new('collection_name').save(@doc, @options)
+      end
+      
+      it "returns the delegates result" do
+        result = 'result'
+        @mongo_collection.stub(:save).and_return(result)
+        MongoDoc::Collection.new('collection_name').save(@doc, @options).should == result
       end
     end
 
