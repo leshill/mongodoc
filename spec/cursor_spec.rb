629f422dea46fa7f1b57c6600f0995797446b5ba
Les Hill <leshill@gmail.com>
2009-12-22 13:03:24 -0500

    Cleanup

--- spec/cursor_spec.rb
+++ spec/cursor_spec.rb
@@ -9,7 +9,7 @@ describe "MongoDoc::Cursor" do
   it ".new wraps a Mongo::Cursor" do
     @cursor._cursor.should == @mongo_cursor
   end
-  
+
   context "with the underlying cursor" do
     %w(close closed? count explain limit query_options_hash query_opts skip sort).each do |delegated_method|
       it "delegates #{delegated_method} to the Mongo::Cursor" do
@@ -18,7 +18,7 @@ describe "MongoDoc::Cursor" do
       end
     end
   end
-  
+
   context "#each" do
     it "delegates to the cursor" do
       @mongo_cursor.should_receive(:each)
@@ -31,7 +31,7 @@ describe "MongoDoc::Cursor" do
       MongoDoc::BSON.should_receive(:decode).with(bson)
       @cursor.each {}
     end
-    
+
     it "calls the block with the decoded return" do
       result = stub('bson')
       @cursor.stub(:_cursor).and_return([result])
@@ -40,39 +40,39 @@ describe "MongoDoc::Cursor" do
       @obj.should == result
     end
   end
-  
+
   context "#next_object" do
     it "delegates to the cursor" do
       @mongo_cursor.should_receive(:next_object)
       @cursor.next_object
     end
-    
+
     it "decodes the return from the delegate" do
       bson = stub('bson')
       @mongo_cursor.stub(:next_object).and_return(bson)
       MongoDoc::BSON.should_receive(:decode).with(bson)
       @cursor.next_object
     end
-    
+
     it "returns nil if the delegate returns nil" do
       @mongo_cursor.stub(:next_object)
       @cursor.next_object.should be_nil
     end
   end
-  
+
   context "#to_a" do
     it "delegates to the cursor" do
       @mongo_cursor.should_receive(:to_a)
       @cursor.to_a
     end
-    
+
     it "decodes the return from the delegate" do
       array = stub('array')
       @mongo_cursor.stub(:to_a).and_return(array)
       MongoDoc::BSON.should_receive(:decode).with(array)
       @cursor.to_a
     end
-    
+
     it "returns [] if the delegate returns []" do
       @mongo_cursor.stub(:to_a).and_return([])
       @cursor.to_a.should == []
