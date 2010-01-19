66159575771bd211ac986862d56d922041cfbb1b
Les Hill <leshill@gmail.com>
2010-01-18 19:17:07 -0500

    Whitespace

--- spec/collection_spec.rb
+++ spec/collection_spec.rb
@@ -8,30 +8,30 @@ describe "MongoDoc::Collection" do
     MongoDoc.should_receive(:database).and_return(db)
     MongoDoc::Collection.mongo_collection(name)
   end
-  
+
   it ".new generates a Mongo::Collection by calling .mongo_collection" do
     name = 'collection_name'
     MongoDoc::Collection.should_receive(:mongo_collection).with(name)
     MongoDoc::Collection.new(name)
   end
-  
+
   context "with the underlying Mongo::Collection" do
     before do
       @mongo_collection = stub('collection')
       MongoDoc::Collection.stub(:mongo_collection).and_return(@mongo_collection)
     end
-    
+
     it "#_collection is the underlying Mongo::Collection" do
       MongoDoc::Collection.new('collection_name')._collection.should == @mongo_collection
     end
-  
+
     %w([] clear count create_index db drop drop_index drop_indexes group hint index_information name options remove rename size).each do |delegated_method|
       it "delegates #{delegated_method} to the Mongo::Collection" do
         @mongo_collection.should_receive(delegated_method)
         MongoDoc::Collection.new('collection_name').send(delegated_method)
       end
     end
-    
+
     context "#find" do
       before do
         @query = { 'sample' => 'data' }
@@ -42,7 +42,7 @@ describe "MongoDoc::Collection" do
         @mongo_collection.stub(:find).and_return(@cursor)
         MongoDoc::Cursor.stub(:new).and_return(@cursor)
       end
-      
+
       it "delegates to the Mongo::Collection" do
         @mongo_collection.should_receive(:find).with(@query, @options, &@block).and_return(@cursor)
         @collection.find(@query, @options, &@block)
@@ -52,13 +52,13 @@ describe "MongoDoc::Collection" do
         MongoDoc::Cursor.should_receive(:new).with(@cursor).and_return(@cursor)
         @collection.find(@query, @options, &@block)
       end
-      
+
       it "calls the block with a wrapped cursor" do
         @collection.find(@query, @options) {|cursor| @result = cursor}
         @result.should == @cursor
       end
     end
-    
+
     context "#find_one" do
       before do
         @spec_or_object_id = { 'sample' => 'data' }
@@ -67,7 +67,7 @@ describe "MongoDoc::Collection" do
         @bson = stub('bson')
         @mongo_collection.stub(:find_one).and_return(@bson)
       end
-      
+
       it "delegates to the Mongo::Collection" do
         @mongo_collection.should_receive(:find_one).with(@spec_or_object_id, @options)
         @collection.find_one(@spec_or_object_id, @options)
@@ -83,7 +83,7 @@ describe "MongoDoc::Collection" do
         MongoDoc::BSON.stub(:decode).and_return(obj)
         @collection.find_one(@spec_or_object_id, @options).should == obj
       end
-      
+
       it "returns nil if the delegate returns nil" do
         @mongo_collection.stub(:find_one)
         @collection.find_one(@spec_or_object_id, @options).should be_nil
@@ -95,12 +95,12 @@ describe "MongoDoc::Collection" do
         @doc = { 'sample' => 'data' }
         @options = {:safe => false}
       end
-      
+
       it "delegates to the Mongo::Collection" do
         @mongo_collection.should_receive(:insert).with(@doc, @options)
         MongoDoc::Collection.new('collection_name').insert(@doc, @options)
       end
-      
+
       it "converts the doc_or_docs to bson" do
         @doc.should_receive(:to_bson)
         @mongo_collection.stub(:insert)
@@ -119,7 +119,7 @@ describe "MongoDoc::Collection" do
         @doc = { 'sample' => 'data' }
         @options = {:safe => false}
       end
-      
+
       it "delegates to the Mongo::Collection" do
         @mongo_collection.should_receive(:save).with(@doc, @options)
         MongoDoc::Collection.new('collection_name').save(@doc, @options)
@@ -130,7 +130,7 @@ describe "MongoDoc::Collection" do
         @mongo_collection.stub(:save)
         MongoDoc::Collection.new('collection_name').save(@doc, @options)
       end
-      
+
       it "returns the delegates result" do
         result = 'result'
         @mongo_collection.stub(:save).and_return(result)
@@ -146,31 +146,31 @@ describe "MongoDoc::Collection" do
         @database = stub('database', :db_command => nil)
         MongoDoc.stub(:database).and_return(@database)
       end
-      
+
       it "delegates to the Mongo::Collection" do
         @mongo_collection.should_receive(:update).with(@spec, @doc, @options)
         MongoDoc::Collection.new('collection_name').update(@spec, @doc, @options)
       end
-      
+
       it "converts the doc to bson" do
         @doc.should_receive(:to_bson)
         @mongo_collection.stub(:update)
         MongoDoc::Collection.new('collection_name').update(@spec, @doc, @options)
       end
-      
+
       it "gets the last error from the database" do
         @mongo_collection.stub(:update)
         @database.should_receive(:db_command).with({'getlasterror' => 1})
         MongoDoc::Collection.new('collection_name').update(@spec, @doc, @options)
       end
-      
+
       it "returns the updateExisting value of get last error" do
         result = 'check'
         @mongo_collection.stub(:update)
         @database.stub(:db_command).and_return({'updatedExisting' => result})
         MongoDoc::Collection.new('collection_name').update(@spec, @doc, @options).should == result
       end
-      
+
       it "returns false otherwise" do
         @mongo_collection.stub(:update)
         MongoDoc::Collection.new('collection_name').update(@spec, @doc, @options).should be_false
