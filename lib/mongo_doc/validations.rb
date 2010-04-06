157fc7caf0d6c3709f972891a6de9dcdde49c5fa
Les Hill <leshill@gmail.com>
2010-04-05 22:19:44 -0400

    Refactor: use ActiveModel if defined

--- lib/mongo_doc/validations.rb
+++ lib/mongo_doc/validations.rb
@@ -1,11 +1,15 @@
-require 'mongo_doc/validations/macros'
-
 module MongoDoc
   module Validations
     def self.included(klass)
       klass.class_eval do
-        include ::Validatable
-        extend Macros
+        if MongoDoc.const_defined?('ActiveModel')
+          include MongoDoc::ActiveModel::ActiveModelCompliance
+        else
+          require 'mongo_doc/validations/macros'
+
+          include ::Validatable
+          extend Macros
+        end
       end
     end
   end
