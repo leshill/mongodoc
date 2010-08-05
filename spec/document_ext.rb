b21230cd79379a965ee3b62b8e495a03a61e8e21
Les Hill <leshill@gmail.com>
2010-08-05 00:07:49 -0400

    Spec cleanup: deprecation warning

--- spec/document_ext.rb
+++ spec/document_ext.rb
@@ -2,7 +2,7 @@ module MongoDoc
   module Document
     def errors_on(attribute)
       self.valid?
-      [self.errors.on(attribute)].flatten.compact
+      [self.errors[attribute]].flatten.compact
     end
     alias :error_on :errors_on
   end
