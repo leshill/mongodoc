261d0b44c924351443c6debc432c4cacb6aeabc8
Les Hill <leshill@gmail.com>
2010-08-28 19:14:48 -0400

    Specfix: add coverage

--- spec/references_many_spec.rb
+++ spec/references_many_spec.rb
@@ -79,7 +79,9 @@ describe MongoDoc::ReferencesMany do
       references_many :addresses, :as => :known_addresses
     end
 
-    subject { Person.new }
+    let(:person) { Person.new }
+
+    subject { person }
 
     context "Object accessor" do
       it { should respond_to(:known_addresses) }
@@ -98,6 +100,41 @@ describe MongoDoc::ReferencesMany do
         Person._keys.should include('known_address_ids')
       end
     end
-  end
 
+    context "setting the ids" do
+      let(:address) { Address.new(:_id => BSON::ObjectID.new) }
+
+      context "to" do
+        before do
+          person.known_addresses = [address]
+        end
+
+        context "nil" do
+          before do
+            person.known_address_ids = nil
+          end
+
+          it "sets the ids to []" do
+            person.known_address_ids.should == []
+          end
+
+          it "resets the objects to nil" do
+            person.known_addresses.should == []
+          end
+        end
+
+        it "[] resets the objects to []" do
+          person.known_address_ids = []
+          person.known_addresses.should == []
+        end
+      end
+
+      context "to strings" do
+        it "converts the strings to ids" do
+          person.known_address_ids = [address._id.to_s]
+          person.known_address_ids.should == [address._id]
+        end
+      end
+    end
+  end
 end
