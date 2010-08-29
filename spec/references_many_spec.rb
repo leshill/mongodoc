abcd787265151e5aa0a01bfba5a9b213f207e0ae
Les Hill <leshill@gmail.com>
2010-08-29 13:26:19 -0400

    Refactor: specs

--- spec/references_many_spec.rb
+++ spec/references_many_spec.rb
@@ -8,13 +8,13 @@ describe MongoDoc::ReferencesMany do
   end
 
   context "Simple Reference" do
-    class Person
+    class PersonSimple
       include MongoDoc::Document
 
       references_many :addresses
     end
 
-    let(:person) { Person.new }
+    let(:person) { PersonSimple.new }
     subject { person }
 
     context "Object accessor" do
@@ -22,7 +22,7 @@ describe MongoDoc::ReferencesMany do
       it { should respond_to(:addresses=) }
 
       it "is not part of the persistent key set" do
-        Person._keys.should_not include('addresses')
+        PersonSimple._keys.should_not include('addresses')
       end
     end
 
@@ -31,55 +31,52 @@ describe MongoDoc::ReferencesMany do
       it { should respond_to(:address_ids=) }
 
       it "is part of the persistent key set" do
-        Person._keys.should include('address_ids')
+        PersonSimple._keys.should include('address_ids')
       end
     end
 
     context "setting the ids" do
       let(:address) { Address.new(:_id => BSON::ObjectID.new) }
 
-      context "to" do
+      before do
+        person.addresses = [address]
+      end
+
+      context "to nil" do
         before do
-          person.addresses = [address]
+          person.address_ids = nil
         end
 
-        context "nil" do
-          before do
-            person.address_ids = nil
-          end
+        its(:address_ids) { should == [] }
+        its(:addresses) { should == [] }
+      end
 
-          it "sets the ids to []" do
-            person.address_ids.should == []
-          end
-
-          it "resets the objects to nil" do
-            person.addresses.should == []
-          end
-        end
-
-        it "[] resets the objects to []" do
+      context "to []" do
+        before do
           person.address_ids = []
-          person.addresses.should == []
         end
+
+        its(:addresses) { should == [] }
       end
 
       context "to strings" do
-        it "converts the strings to ids" do
+        before do
           person.address_ids = [address._id.to_s]
-          person.address_ids.should == [address._id]
         end
+
+        its(:address_ids) { should == [address._id] }
       end
     end
   end
 
   context "Named Reference" do
-    class Person
+    class PersonNamed
       include MongoDoc::Document
 
       references_many :addresses, :as => :known_addresses
     end
 
-    let(:person) { Person.new }
+    let(:person) { PersonNamed.new }
 
     subject { person }
 
@@ -88,7 +85,7 @@ describe MongoDoc::ReferencesMany do
       it { should respond_to(:known_addresses=) }
 
       it "is not part of the persistent key set" do
-        Person._keys.should_not include('known_addresses')
+        PersonNamed._keys.should_not include('known_addresses')
       end
     end
 
@@ -97,43 +94,40 @@ describe MongoDoc::ReferencesMany do
       it { should respond_to(:known_address_ids=) }
 
       it "is part of the persistent key set" do
-        Person._keys.should include('known_address_ids')
+        PersonNamed._keys.should include('known_address_ids')
       end
     end
 
     context "setting the ids" do
       let(:address) { Address.new(:_id => BSON::ObjectID.new) }
 
-      context "to" do
+      before do
+        person.known_addresses = [address]
+      end
+
+      context "to nil" do
         before do
-          person.known_addresses = [address]
+          person.known_address_ids = nil
         end
 
-        context "nil" do
-          before do
-            person.known_address_ids = nil
-          end
+        its(:known_address_ids) { should == [] }
+        its(:known_addresses) { should == [] }
+      end
 
-          it "sets the ids to []" do
-            person.known_address_ids.should == []
-          end
-
-          it "resets the objects to nil" do
-            person.known_addresses.should == []
-          end
-        end
-
-        it "[] resets the objects to []" do
+      context "to []" do
+        before do
           person.known_address_ids = []
-          person.known_addresses.should == []
         end
+
+        its(:known_addresses) { should == [] }
       end
 
       context "to strings" do
-        it "converts the strings to ids" do
+        before do
           person.known_address_ids = [address._id.to_s]
-          person.known_address_ids.should == [address._id]
         end
+
+        its(:known_address_ids) { should == [address._id] }
       end
     end
   end
